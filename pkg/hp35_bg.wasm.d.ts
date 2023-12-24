9cdf41861bdcd4c1f42eb1cacc79e8977391fc20
Veniamin Ilmer <ven@ven.com>
2023-12-24 13:02:25 -0500

    Forgot to add in binary

---
+++ pkg/hp35_bg.wasm.d.ts
@@ -0,0 +1,11 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function run(): number;
+export function __wbindgen_malloc(a: number, b: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export function wasm_bindgen__convert__closures__invoke1_mut__hd3be11cd40d19e87(a: number, b: number, c: number): void;
+export function __wbindgen_free(a: number, b: number, c: number): void;
+export function __wbindgen_exn_store(a: number): void;
+export function wasm_bindgen__convert__closures__invoke2_mut__h1601cd64db6cd96c(a: number, b: number, c: number, d: number): void;
